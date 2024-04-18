231548b5fadcd0b7b22b0ab6b3130f55a7a695fc
Mark Blumenthal <markblumenthal@live.com>
2024-04-18 14:43:42 +0300

    changes

--- lets-get-fat/pg.js
+++ lets-get-fat/pg.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
+const path = require('path'); 
 const port = 3000;
 
 // Database connection
@@ -12,12 +13,26 @@ const pool = new Pool({
     password: '************', 
     port: 5432,
   });
-  
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Let\'s Get Fat!');
+// Serve static files from the "lets-get-fat" directory
+app.use(express.static(path.join(__dirname, 'lets-get-fat'))); 
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'lets-get-fat', 'landingpage.html'));
 });
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+  
+
+
+
+
+
+
 
 
--- lets-get-fat/server.js
+++ lets-get-fat/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const path = require('path'); 
 const port = 3000;
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Let\'s Get Fat!');
+// Serve static files from the "lets-get-fat" directory
+app.use(express.static(path.join(__dirname, 'lets-get-fat'))); 
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'lets-get-fat', 'landingpage.html'));
 });
 
 app.listen(port, () => {

8425c175fa2ec6bac8cba5ce4ec3c3c058fa2015
Mark Blumenthal <markblumenthal@live.com>
2024-04-18 15:03:23 +0300

    changes

--- lets-get-fat/server.js
+++ lets-get-fat/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
 const path = require('path'); 
 const port = 3000;
+
+// Database connection
+const pool = new Pool({
+    user: 'tasks_owner', 
+    host: 'ep-twilight-queen-a24z353q.eu-central-1.aws.neon.tech', 
+    database: 'letsgetfat', 
+    password: '************', 
+    port: 5432,
+});
 
 app.use(express.json()); // for parsing application/json
 
@@ -10,7 +20,7 @@ app.use(express.json()); // for parsing application/json
 app.use(express.static(path.join(__dirname, 'lets-get-fat'))); 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'lets-get-fat', 'landingpage.html'));
+    res.sendFile(path.join(__dirname, 'landingpage.html'));
 });
 
 app.listen(port, () => {

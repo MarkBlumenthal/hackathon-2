a7896cd96f8ec24f71ebf432f6d1ebc11c2ee390
Mark Blumenthal <markblumenthal@live.com>
2024-04-18 17:47:22 +0300

    changes

--- lets-get-fat/server.js
+++ lets-get-fat/server.js
@@ -10,19 +10,25 @@ const pool = new Pool({
     user: 'tasks_owner', 
     host: 'ep-twilight-queen-a24z353q.eu-central-1.aws.neon.tech', 
     database: 'letsgetfat', 
-    password: '************', 
+    password: '************',  
     port: 5432,
 });
 
-app.use(express.json()); // for parsing application/json
 
-// Serve static files from the "lets-get-fat" directory
-app.use(express.static(path.join(__dirname, 'lets-get-fat'))); 
+app.use(express.json());
 
+
+app.use(express.static(__dirname)); 
+
+// Root route to serve the landing page
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'landingpage.html'));
 });
 
+
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
+
+

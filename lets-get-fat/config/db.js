90304ce26696046055529d2aea8b9e60c6c81207
Mark Blumenthal <markblumenthal@live.com>
2024-04-19 17:05:11 +0300

    changes

--- lets-get-fat/config/db.js
+++ lets-get-fat/config/db.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'ep-twilight-queen-a24z353q.eu-central-1.aws.neon.tech',
-    database: 'letsgetfat',
-    user: 'tasks_owner',
-    password: 'krnmxyAqa5e1',
-    port: 5432, // Default PostgreSQL port
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
     ssl: {
-        rejectUnauthorized: false // Necessary if your PG server uses SSL
+        rejectUnauthorized: false 
     }
 });
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
 };
+
